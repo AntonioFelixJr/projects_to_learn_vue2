b51dd98e3cf4ba00160da5a694d5e02e83397db6
Antonio Felix <antonio.felixjr@hotmail.com>
2020-06-30 08:57:35 -0300

    Componentes

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Contador</h1>
+        <app-contadores></app-contadores>
+        <button>?</button>
+    </div>
+  
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
