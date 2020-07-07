d8a7af645f11ec53bdb107fa2126ae69526e61d3
Antonio Felix <antonio.felixjr@hotmail.com>
2020-07-07 08:53:15 -0300

    Componentes Avançado

---
+++ componentes-avancado/citacoes/src/components/Citacao.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="citacao">
+        <slot name="fonte"></slot>
+        <slot></slot>
+        <div class="autor">
+            <slot name="autor"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+
+    .citacao h1 {
+        color: black;
+    }
+
+    .autor {
+        float: right;
+    }
+</style>
